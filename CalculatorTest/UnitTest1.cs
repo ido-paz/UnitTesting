6e56e0c71784af2c06869ffa2c1fce5a12afbfa3
ido_p_a@hotmail.com <ido_p_a@hotmail.com>
2021-04-08 15:29:04 +0300

    fix #2

---
+++ CalculatorTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class FunctionalityTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
