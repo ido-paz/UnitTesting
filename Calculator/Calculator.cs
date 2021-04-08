5f6b36d28a6601b5208bbfb0ebdeb6b0093a549e
ido_p_a@hotmail.com <ido_p_a@hotmail.com>
2021-04-08 16:42:34 +0300

    fix #5, fix #8

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -5,5 +5,7 @@ namespace Calculator
     public class Calculator
     {
         public float Multiply(float a, float b) => a * b;
+
+        public float Divide(float a, float b) => a / b;
     }
 }
--- CalculatorTest/FunctionalityTests.cs
+++ CalculatorTest/FunctionalityTests.cs
@@ -9,7 +9,14 @@ namespace CalculatorTest
         public void Test_Multiply_Result()
         {
             Calculator.Calculator calculator = new Calculator.Calculator();
-            Assert.AreEqual(calculator.Multiply(2, 4), 81);
+            Assert.AreEqual(calculator.Multiply(2, 4), 8);
+        }
+
+        [TestMethod]
+        public void Test_Divide_Result()
+        {
+            Calculator.Calculator calculator = new Calculator.Calculator();
+            Assert.AreEqual(calculator.Divide(4, 2), 2);
         }
 
     }
